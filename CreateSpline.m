26bbd67c312bd7a9c518e88b0f29fd5d7036002b
Michael31415 <mike@mbox.in.ua>
2016-12-08 16:41:39 +0200

    Stub for creating spline

---
+++ CreateSpline.m
@@ -0,0 +1,15 @@
+function [interpolationSpline] = CreateSpline(points, func, degree, fault = 1, SECreationMethod = 0, SESolvingMethod = 0)
+	if strcmp(class(func), 'function_handle')
+		values = arrayfun(func, points);
+	elseif length(func) == length(points)
+		values = func;
+	else
+		error('Unknown format of input argument func.');
+	end;
+	if isrow(values)
+		values = values';
+	end;
+	matrix = CreateSEMatrix(points, func, degree, SECreationMethod);
+	solution = SolveSE(matrix, SESolvingMethod);
+	interpolationSpline = FromSpline(points, func, degree, fault, matrix, SECreationMethod);
+end;
